96599ec887303e34e096b58f8ac82426eaff69bb
Renato Lessa da Silva <renatolessa.2011@hotmail.com>
2018-10-19 15:36:35 -0300

    Testando Assertivas

--- src/test/java/br/relesi/rlessa/servicos/LocacaoServiceTest.java
+++ src/test/java/br/relesi/rlessa/servicos/LocacaoServiceTest.java
@@ -1,7 +1,15 @@
 package br.relesi.rlessa.servicos;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Date;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,12 +32,17 @@ public class LocacaoServiceTest {
 		// acao
 		Locacao locacao = service.alugarFilme(usuario, filme);
 
-		// verificacao
 		
-		Assert.assertEquals(5.0,locacao.getValor(), 0.01);		
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		//verificacao
+		assertThat(locacao.getValor(), is(equalTo(5.0)));
+		assertThat(locacao.getValor(), is(not(6.0)));
+		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
 
 	}
+
+
+
+	
 	
 }
